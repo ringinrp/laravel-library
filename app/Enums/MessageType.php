ac670cf9d15d2cee0aa70a9b33cbc25a930c780a
ringinrp <ringinrp21@gmail.com>
2024-11-02 13:38:20 +0700

    first commit

---
+++ app/Enums/MessageType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enums;
+
+enum MessageType: string
+{
+    case CREATED = 'Berhasil Menambahkan';
+    case UPDATED = 'Berhasil Memperbaharui';
+    case DELETED = 'Berhasil Menghapus';
+    case ERROR - 'Terjadi Kesalahan'
+
+    public static message(string $entity = '', ?string $error = null): string
+
+    {
+        if($this === MessageType :: ERROR && $error){
+            return '{$this->value} {error}';
+        }
+
+        return "{$this->value} {$entity}";
+    }
+
+}
