1f3476fad7faeb8a011080f8c3a7f6b723c642b6
ringinrp <ringinrp21@gmail.com>
2024-11-03 12:26:59 +0700

    feat(return book): membuat model dan migration return book

---
+++ app/Models/ReturnBookCheck.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ReturnBookCondition;
+use Illuminate\Database\Eloquent\Model;
+
+class ReturnBookCheck extends Model
+{
+    protected $fillable = [
+        'return_book_id',
+        'condition',
+        'notes'
+    ];
+
+    public function casts():array{
+        return [
+            'condition' => ReturnBookCondition::class,
+        ];
+    }
+}
