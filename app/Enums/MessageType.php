e2e94718e2f2736cf50b29c65db29ce417dffa7b
ringinrp <ringinrp21@gmail.com>
2024-11-03 07:31:01 +0700

    fix(enum): memperbaiki enum message type

--- app/Enums/MessageType.php
+++ app/Enums/MessageType.php
@@ -7,12 +7,12 @@ enum MessageType: string
     case CREATED = 'Berhasil Menambahkan';
     case UPDATED = 'Berhasil Memperbaharui';
     case DELETED = 'Berhasil Menghapus';
-    case ERROR - 'Terjadi Kesalahan'
+    case ERROR = 'Terjadi Kesalahan';
 
-    public static message(string $entity = '', ?string $error = null): string
+    public function message(string $entity = '', ?string $error = null): string
 
     {
-        if($this === MessageType :: ERROR && $error){
+        if($this === MessageType::ERROR && $error){
             return '{$this->value} {error}';
         }
 
