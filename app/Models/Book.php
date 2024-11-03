e329f627c20ef8da0550a089d143d15cd9292968
ringinrp <ringinrp21@gmail.com>
2024-11-03 07:33:45 +0700

    feat(model): menambahkan model buku, category, publisher

---
+++ app/Models/Book.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'book_code',
+        'title',
+        'slug',
+        'author',
+        'publication_year',
+        'isbn',
+        'language',
+        'synopsis',
+        'number_of_pages',
+        'status',
+        'cover',
+        'category_id',
+        'publisher_id',
+        'category_id',
+        'price'
+    ];
+
+    public function casts():array{
+        return [
+            'language' => BookLanguage::class,
+            'status' => BookStatus::class
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Enums\UserGender;
 
 class User extends Authenticatable
 {
@@ -20,6 +21,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'address',
+        'username',
+        'avatar',
+        'gender',
+        'date_of_birth',
         'password',
     ];
 
@@ -43,6 +49,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'gender' => UserGender
         ];
     }
 }
