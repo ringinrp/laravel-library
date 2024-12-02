84e2feef62c1929f3c99c95e7ea07a66b5ac144c
ringinrp <ringinrp21@gmail.com>
2024-12-02 15:15:54 +0700

    feat(seeder): make seeder category, publisher

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->create(attributes: [
+            'name' => 'Pratama Harun',
+            'username' => 'pratama',
+            'email' => 'pratama@gmail.com',
+            'password' => Hash::make('password') //password bisa saja tidak diisi karena sudah default pada factories
         ]);
+
+        $this->call(class : CategorySeeder::class);
+        $this->call(class: PublisherSeeder::class);
     }
 }
