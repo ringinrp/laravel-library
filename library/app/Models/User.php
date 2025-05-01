38468c9ca531de6c33215a655f62a85aca17f9e7
ringinrp <ringinrp21@gmail.com>
2025-05-01 20:03:58 +0700

    update: migration, model user

--- library/app/Models/User.php
+++ library/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\UserGender;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +23,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'phone',
+        'avatar',
+        'gender',
+        'date_of_birth',
+        'address',
     ];
 
     /**
@@ -43,6 +51,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'gender' => UserGender::class
         ];
     }
 }
