0582dfee7ec433abdb1e361f53aed7f08d96181c
ringinrp <ringinrp21@gmail.com>
2025-05-12 20:45:37 +0700

    update: components

--- library/resources/js/Components/InputError.jsx
+++ library/resources/js/Components/InputError.jsx
@@ -2,7 +2,7 @@ export default function InputError({ message, className = '', ...props }) {
     return message ? (
         <p
             {...props}
-            className={'text-sm text-red-600 ' + className}
+            className={cn('text-xs font-medium text-red-600 ', className)}
         >
             {message}
         </p>
