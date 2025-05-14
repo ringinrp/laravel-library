e68d7a0dff088bb21a9092c19d03d40f048daba2
ringinrp <ringinrp21@gmail.com>
2025-05-14 16:52:09 +0700

    update: utils

--- library/resources/js/lib/utils.js
+++ library/resources/js/lib/utils.js
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+
+export const FINEPAYMENTSTATUS = {
+    PENDING: "Tertunda",
+    SUCCESS: "Sukses",
+    FAILED: "Gagal"
+}
