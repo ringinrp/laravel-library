211e7372a51cd7a087549c74797115b2f4d6e0be
ringinrp <ringinrp21@gmail.com>
2025-06-08 11:53:35 +0700

    update : category controller, resource

--- library/app/Http/Controllers/Admin/CategoryController.php
+++ library/app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Inertia\Response;
@@ -16,7 +17,7 @@ class CategoryController extends Controller
             ->get();
 
         return inertia('Admin/Categories/Index', [
-            'categories' => $categories,
+            'categories' => CategoryResource::collection($categories),
             'page_settings' => [
                 'title' => 'Kategori',
                 'subtitle' => 'Menampilkan semua data kategori yang tersedia pada platform ini',
