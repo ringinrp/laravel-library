b55d4b46bb76b278476dfd9a7b3b380563d8435c
ringinrp <ringinrp21@gmail.com>
2025-06-16 09:30:15 +0700

    update : stock book

---
+++ library/app/Observers/BookObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Book;
+
+class BookObserver
+{
+    public function created(Book $book): void
+    {
+        $book->stock()->create([
+            'total' => $total = request()->total,
+            'available' => $total,
+        ]);
+    }
+
+    //custome sendiri
+    public function updated(Book $book): void
+    {
+        // Menghapus stok sebelumnya sebelum menambah data baru
+        $book->stock()->delete();
+
+        // Menambahkan stok baru setelah dihapus
+        $total = request()->total;
+
+        $book->stock()->create([
+            'total' => $total,
+            'available' => $total,
+        ]);
+    }
+}
