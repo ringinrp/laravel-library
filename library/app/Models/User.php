9cd2182c7ab299b86c235e9f8976a6fdda299fe7
ringinrp <ringinrp21@gmail.com>
2025-07-12 11:33:26 +0700

    update : install role permission

--- library/app/Models/User.php
+++ library/app/Models/User.php
@@ -15,7 +15,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
